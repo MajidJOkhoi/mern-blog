bff2810ec0782de067c61859367234f7e13f58ef
majid ali <majidalijkhio@gmail.com>
2024-06-09 09:52:11 +0500

    create pages routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Project from "./pages/Project";
+import Singin from "./pages/Singin";
+import Singup from "./pages/Singup";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold italic text-center">
-      Hello world!
-    </h1>
-  )
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/sign-in" element={<Singin/>} />
+        <Route path="/sign-up" element={<Singup />} />
+      </Routes>
+  
+  );
 }
