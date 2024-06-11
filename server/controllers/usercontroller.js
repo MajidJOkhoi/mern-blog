3d2324872988d648ff1ca767f8c8fb7ed5013a68
majid ali <majidalijkhio@gmail.com>
2024-06-11 09:05:25 +0500

    create test api router and controller

---
+++ server/controllers/usercontroller.js
@@ -0,0 +1,7 @@
+const test = (req, res) => {
+  res.send("Api  is working .... ");
+};
+
+module.exports = {
+  test,
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,18 +1,10 @@
 const express = require('express');
-const User = require('../models/user');
+const { test } = require('../controllers/usercontroller');
 
 const router = express.Router();
 
 // Route to create a new user
-router.post('/users', async (req, res) => {
-    const { name, email } = req.body;
-    try {
-        const user = new User({ name, email });
-        await user.save();
-        res.status(201).json(user);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
+
+router.get("/test",test)
 
 module.exports = router;
